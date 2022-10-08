64bfe6522f9b0c2792b276ca1890c22664723d99
hybfkuf <fredjoseph863@gmail.com>
2022-10-08 09:02:54 +0800

    Add subcommand zoocfg to export zookeeper zoo.cfg, add subcommand kfcfg to exporter kafka server.properties

--- kafka/docker-compose/zk-multiple-kafka-multiple/exec.sh
+++ kafka/docker-compose/zk-multiple-kafka-multiple/exec.sh
@@ -8,6 +8,12 @@ kafka|kf)
 zookeeper|zoo)
     docker exec -it $(docker ps | grep 2181 | awk '{print $1}') bash
     ;;
+kfcfg)
+    docker exec -it $(docker ps | grep 9092 | awk '{print $1}') cat /opt/bitnami/kafka/config/server.properties > /tmp/server.properties
+    ;;
+zoocfg)
+    docker exec -it $(docker ps | grep 2181 | awk '{print $1}') cat /opt/bitnami/zookeeper/conf/zoo.cfg > /tmp/zoo.cfg
+    ;;
 *)
     echo "Usage: $(basename $0) zookeeper|kafka"
 esac
