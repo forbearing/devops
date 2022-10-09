4d3cd879262bc35a2c4551ca02e2483069eb345e
hybfkuf <fredjoseph863@gmail.com>
2022-10-09 08:37:33 +0800

    Update: comment out `KAFKA_CFG_SASL_ENABLED_MECHANISMS: PLAIN`, `KAFKA_CFG_SASL_MECHANISM_INTER_BROKER_PROTOCOL: PLAINTEXT`

--- kafka/docker-compose/zk-multiple-kafka-multiple/exec.sh
+++ kafka/docker-compose/zk-multiple-kafka-multiple/exec.sh
@@ -3,16 +3,16 @@
 
 case $1 in
 kafka|kf)
-    docker exec -it $(docker ps | grep 9092 | awk '{print $1}') bash
+    docker exec -it $(docker ps | grep '/kafka' | awk '{print $1}') bash
     ;;
 zookeeper|zoo)
-    docker exec -it $(docker ps | grep 2181 | awk '{print $1}') bash
+    docker exec -it $(docker ps | grep '/zookeeper' | awk '{print $1}') bash
     ;;
 kfcfg)
-    docker exec -it $(docker ps | grep 9092 | awk '{print $1}') cat /opt/bitnami/kafka/config/server.properties > /tmp/server.properties
+    docker exec -it $(docker ps | grep '/kafka' | awk '{print $1}') cat /opt/bitnami/kafka/config/server.properties > /tmp/server.properties
     ;;
 zoocfg)
-    docker exec -it $(docker ps | grep 2181 | awk '{print $1}') cat /opt/bitnami/zookeeper/conf/zoo.cfg > /tmp/zoo.cfg
+    docker exec -it $(docker ps | grep '/zookeeper' | awk '{print $1}') cat /opt/bitnami/zookeeper/conf/zoo.cfg > /tmp/zoo.cfg
     ;;
 *)
     echo "Usage: $(basename $0) zookeeper|kafka"
--- kafka/docker-compose/zk-multiple-kafka-multiple/get-controller.sh
+++ kafka/docker-compose/zk-multiple-kafka-multiple/get-controller.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec -it $(docker ps | grep 2181 | awk '{print $1}') zkCli.sh get /kafka/controller
+docker exec -it $(docker ps | grep '/zookeeper' | awk '{print $1}') zkCli.sh get /kafka/controller
